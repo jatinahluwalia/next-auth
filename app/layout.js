f27c97b1e763c88b8fb1599d62080edb13b04355
jitu2404 <103820854+jitu2404@users.noreply.github.com>
2023-07-24 13:26:04 +0530

    auth

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,8 @@
+"use client"
+
 import './globals.css'
 import { Inter } from 'next/font/google'
-
+import { SessionProvider, useSession } from 'next-auth/react'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
@@ -9,9 +11,12 @@ export const metadata = {
 }
 
 export default function RootLayout({ children }) {
+
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <SessionProvider>
+        <body className={inter.className}>{children}</body>
+      </SessionProvider>
     </html>
   )
 }
